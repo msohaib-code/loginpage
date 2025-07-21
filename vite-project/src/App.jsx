340fd55ed8bb9c88f8bf4f0a07d8dd354b4f2acc
msohaib-code <muhammad.sohaib@hubextech.com>
2025-07-21 19:40:00 +0500

    remove commit app.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import { BrowserRouter, Routes, Route, } from 'react-router-dom';
-// import Loginpage from './screen/Loginpage'
-// import Signuppage from './screen/signuppage'
-// const App = () => {
-//   return (
-//     <div>
-
-
-//       <Routes>
-//         <Route path="/" element={<Signuppage />} />
-//         <Route path="/login" element={<Loginpage />} />
-//         <Route path="/signup" element={<Signuppage />} />
-//       </Routes>
-   
-//     </div>
-//   )
-// }
-
-// export default App
 
 
 
